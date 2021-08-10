394dcd87c575a29c2fff1f6817863c8720377f88
Jane Lu <jyl.janelu@gmail.com>
2021-08-09 21:42:39 -0700

    seeds

---
+++ seeds/pets-seeds.js
@@ -0,0 +1,15 @@
+const { Pet } = require("../models/Pets");
+
+const petData = [
+  {
+    pet_name: "Cookie",
+    id: 0123,
+    birthday: 0210, //how to get years
+    health_condition: "healthy",
+    gender: "female",
+  },
+];
+
+const seedPets = () => Pets.bulkCreate(petsData);
+
+module.exports = seedPets;
