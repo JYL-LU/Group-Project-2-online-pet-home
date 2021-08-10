c76c522ebf4bc6b12e7bbd65e6d9d84dbc85136b
Jane Lu <jyl.janelu@gmail.com>
2021-08-09 21:39:59 -0700

    model pets and seed pets

---
+++ models/Pets.js
@@ -0,0 +1,49 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Pets extends Model {}
+
+// define table columns and configuration
+Pets.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+
+    pet_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    birthday: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true,
+      },
+    },
+
+    heath_condition: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    gender: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "Pets",
+  }
+);
+
+module.exports = Pets;
