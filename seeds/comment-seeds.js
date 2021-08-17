e47695621827f51a7a5d05c95241d3648c316833
hunterD3CD <hunterd3c@gmail.com>
2021-08-17 12:14:01 -0700

    css update

--- seeds/comment-seeds.js
+++ seeds/comment-seeds.js
@@ -2,22 +2,22 @@ const { Comment } = require("../models");
 
 const commentData = [
   {
-    comment_text: "this homeless dog is under urgent condiction",
+    comment_text: "she's doing very well! She is off medication and is no longer a shy dog! ",
     user_id: 1,
     post_id: 1,
   },
   {
-    comment_text: "this homeless dog is feeded today",
+    comment_text: "Red is certainly a connoisseur of toys, with no lack of love for each and every one.",
     user_id: 2,
     post_id: 2,
   },
   {
-    comment_text: "this homeless dog is moved to the shelter",
+    comment_text: "Bao does have some joint pain, especially in the back end.",
     user_id: 3,
     post_id: 3,
   },
   {
-    comment_text: "this homeless dog's leg is under bad condition",
+    comment_text: "this homeless dog is just moved to the shelter",
     user_id: 4,
     post_id: 4,
   },
--- seeds/post-seeds.js
+++ seeds/post-seeds.js
@@ -3,35 +3,37 @@ const { Post } = require("../models");
 const postData = [
   {
     title: "Cookie",
-    content: "one eye is blind",
+    content:
+      "Cookie is a gentle soul. Made for extreme couch snuggles, rolling around on your lap and wiggles. She adores people. People are her best thing and all she wants is to be at home with them. She is polite and courteous of her toys and food. Also perfectly housetrained.",
     address: "1st ave, Seattle",
     image_url: "https://images.dog.ceo/breeds/groenendael/n02105056_6567.jpg",
     user_id: 1,
   },
   {
     title: "Red",
-    content: "healthy",
+    content: "This handsome, quirky-eared guy is looking for a best-bud. Someone who can hang, go on long walks with and play some ball!",
     address: "e ave, San Francisco",
     image_url: "https://images.dog.ceo/breeds/pekinese/n02086079_11089.jpg",
     user_id: 2,
   },
   {
     title: "Bao",
-    content: "healthy",
+    content: "Bao is in a foster home, and is currently not at the shelter. Please find info below on how to adopt him, thanks!",
     address: "w ave, San Francisco",
-    image_url: "https://images.dog.ceo/breeds/ovcharka-caucasian/IMG_20190611_152047.jpg",
+    image_url:
+      "https://images.dog.ceo/breeds/ovcharka-caucasian/IMG_20190611_152047.jpg",
     user_id: 3,
   },
   {
     title: "Joke",
-    content: "healthy",
+    content: "He will need an adult only home (ages 12+) as he can be a bit skeptical of new people. In addition, Joke will need continued guidance on helping to build his confidence around new folks. Treats help lots!",
     address: "1st ave, Seattle",
     image_url: "https://images.dog.ceo/breeds/pyrenees/n02111500_2736.jpg",
     user_id: 3,
   },
   {
     title: "Brown",
-    content: "healthy",
+    content: "We're still getting to know Brown, so please check back later for more information",
     address: "4th ave, Seattle",
     image_url: "https://images.dog.ceo/breeds/komondor/n02105505_3723.jpg",
     user_id: 4,
